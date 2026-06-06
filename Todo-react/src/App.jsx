d7e8dcaabd3443109c4814815fe885377fd7e48b
Puoch Onwar <pouch0200@gmail.com>
2026-06-06 18:36:51 +0200

    inital commit

---
+++ Todo-react/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Main1 from './Main1'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Main1 />
+      
+    </>
+  );
+}
+
+export default App
---
+++ Todo-react/src/Main1.jsx
@@ -0,0 +1,79 @@
+// import { use, useState } from "react";
+
+// export default function Main1() {
+//   const [addTask, SetTask] = useState(0)
+//   const [deleteTask, Setdelete] = useState('')
+//   function add() {
+
+//     }
+
+//   return (
+//       <>
+//         <h1 className="m-5 text-8xl text-gray-200 font-semibold flex justify-center items-center">
+//           todos
+//         </h1>
+//         <div className="flex justify-center  ">
+//           <input
+//             className=" w-3xl h-12 border rounded-l-3xl rounded-r-3xl text-2xl font-semibold pl-5 "
+//             type="text"
+//             placeholder="Add To Do..."
+//           />
+//           <button className="bg-blue-400 p-2 pr-2 pl-2 rounded-l-2xl rounded-r-2xl">Add</button>
+//         </div>
+//       </>
+//     );
+// }
+
+import { useState } from "react";
+
+export default function Main1() {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  function add() {
+    if (task.trim() === "") return;
+
+    setTasks((prev) => [...prev, task]);
+    setTask("");
+  }
+
+  function deleteTask(index) {
+    setTasks((prev) => prev.filter((_, i) => i !== index));
+  }
+
+  return (
+    <>
+      <h1 className="m-5 text-8xl text-gray-200 font-semibold flex justify-center items-center">
+        todos
+      </h1>
+
+      <div className="flex justify-center">
+        <input
+          className="w-3xl h-12 border rounded-3xl text-2xl  pl-5"
+          type="text"
+          placeholder="Add To Do..."
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+        />
+
+        <button onClick={add} className="bg-blue-400 p-2 rounded-2xl ml-2">
+          Add
+        </button>
+      </div>
+
+      <div className="flex flex-col items-center mt-5">
+        {tasks.map((item, index) => (
+          <div
+            key={index}
+            className="flex justify-between w-96 bg-gray-200 p-2 m-2 rounded"
+          >
+            <li>{item}</li>
+            <button onClick={() => deleteTask(index)}>
+              Delete
+            </button>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
